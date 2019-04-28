8aa51ca5c06a447489af40ab1ac74980b8da6e7d
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2019-04-27 19:24:15 -0500

    Added child comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildOneComponent } from './components/child-one/child-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildOneComponent
   ],
   imports: [
     BrowserModule
